46a55cad4368ed92286db12372fd39b151f5bf01
Emily Tran <tran.thn@gmail.com>
2020-09-08 15:29:33 -0400

    minor data cleanup, initial implementation of winnow2

--- project1/load-data.py
+++ project1/load-data.py
@@ -4,6 +4,7 @@ import csv
 from fractions import Fraction
 import numpy as np
 import pandas as pd
+import winnow2 as win
 
 breast = './data/breast-cancer-wisconsin.data'
 glass = './data/glass.data'
@@ -39,70 +40,71 @@ def split_tuning_training_data(df):
     tuning = df.sample(frac = 0.1)
     df = df.drop(tuning.index)
     train = df.sample(frac = 0.6)
-    print(train)
     test = df.drop(train.index)
     return {'tuning': tuning, 'train': train, 'test': test}
 
 ############### main ###############
-## last column = malignancy (2 = benign, 4 = malignant)
+## class:  2 options (2 = benign, 4 = malignant) - remap to 0 = benign, 1 malignant
 ## missing: 16 rows - missing 1 column value for bare_nuclei
 ## each column has domain: 1-10, need to be binned (except sample-code-number, class)
 breast_fields = ['sample-code-number','clump-thickness','uniformity-of-cell-size','uniformity-of-cell-shape','marginal-adhesion','single-epithelial-cell-size','bare-nuclei','bland-chromatin','normal-nucleoli','mitoses','class']
 bin_fields = breast_fields[1:-1]
 bdf = read_csv(breast, breast_fields)
+bdf = bdf.replace({'class': {4: 1, 2: 0}})
 bdf2 = bdf[bdf['bare-nuclei'] != '?']
 bdf2 = bdf2.astype({ 'bare-nuclei': int })
 bdf3 = bdf2.copy()
 bin_continuous(bdf3, bin_fields)
-bdf4 = pd.get_dummies(bdf3)
 
-# print('data length', len(bdf4))
-# data_sets = split_tuning_training_data(bdf4)
-# print('data length, split', len(data_sets['tuning']), len(data_sets['train']), len(data_sets['test']))
-# print('---')
+# drop sample-code-number since it not needed for learning model
+bdf4 = pd.get_dummies(bdf3).drop(columns = 'sample-code-number')
+data_sets = split_tuning_training_data(bdf4)
 
+# for initial testing, let's use a static subset of data
+win.build_table(bdf4)
+
+########################################
 ## attribute values need values binned into ranges (except id, type)
 ## missing: none
-glass_fields = ['id','ri','na','mg', 'al','si','k','ca','ba','fe','type']
+## class: 7 options
+glass_fields = ['id','ri','na','mg', 'al','si','k','ca','ba','fe','class']
 bin_fields = glass_fields[1:-1]
 gdf = read_csv(glass, glass_fields)
 gdf2 = gdf.copy()
 bin_continuous(gdf2, bin_fields)
-gdf3 = pd.get_dummies(gdf2)
-print('data length', len(gdf3))
+gdf3 = pd.get_dummies(gdf2).drop(columns = 'id')
 data_sets = split_tuning_training_data(gdf3)
-print('data length, split', len(data_sets['tuning']), len(data_sets['train']), len(data_sets['test']))
-print('---')
+# print(data_sets['tuning'])
 
+########################################
 ## attribute values need values binned into ranges (except class)
 ## missing: none
+## class: 3 options
 iris_fields = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 bin_fields = iris_fields[:-1]
 irdf = read_csv(iris, iris_fields)
 irdf2 = irdf.copy()
 bin_continuous(irdf2, bin_fields)
-print('data length', len(irdf2))
 data_sets = split_tuning_training_data(irdf2)
-print('data length, split', len(data_sets['tuning']), len(data_sets['train']), len(data_sets['test']))
-print('---')
+# print(data_sets['tuning'])
 
+########################################
 ## attribute values are either multi-categorical or binary (assuming no missing)
 ## missing: none
+## class: 4 options
 soybean_fields = ['date','plant-stand','precip','temp','hail','crop-hist','area-damaged','severity','seed-tmt','germination','plant-growth','leaves','leafspots-halo','leafspots-marg','leafspot-size','leaf-shread','leaf-malf','leaf-mild','stem','lodging','stem-cankers','canker-lesion','fruiting-bodies','external decay','mycelium','int-discolor','sclerotia','fruit-pods','fruit spots','seed','mold-growth','seed-discolor','seed-size','shriveling','roots', 'class']
 soydf = read_csv(soybean, soybean_fields)
 soydf2 = pd.get_dummies(soydf)
-print('data length', len(soydf2))
 data_sets = split_tuning_training_data(soydf2)
-print('data length, split', len(data_sets['tuning']), len(data_sets['train']), len(data_sets['test']))
-print('---')
+# print(data_sets['tuning'])
 
+########################################
 ## attribute values are either multi-categorical or binary (assuming no missing)
 ## ? = abstain, not missing values
 ## missing: none
+## class: 2 options
 house_fields = ['class', 'handicapped-infants', 'water-project-cost-sharing', 'adoption-of-the-budget-resolution', 'physician-fee-freeze', 'el-salvador-aid', 'religious-groups-in-schools', 'anti-satellite-test-ban', 'aid-to-nicaraguan-contras', 'mx-missile', 'immigration', 'synfuels-corporation-cutback', 'education-spending', 'superfund-right-to-sue', 'crime', 'duty-free-exports', 'export-administration-act-south-africa']
 housedf = read_csv(house, house_fields)
 housedf2 = pd.get_dummies(housedf)
-print('data length', len(housedf))
 data_sets = split_tuning_training_data(housedf)
-print('data length, split', len(data_sets['tuning']), len(data_sets['train']), len(data_sets['test']))
-print('---')
+# print(data_sets['tuning'])
--- project1/winnow2.py
+++ project1/winnow2.py
@@ -5,7 +5,51 @@ import numpy as np
 import pandas as pd
 
 ## winnow steps
-## with tuning set, experiment with
-##  - weight vectors
-##  - theta: threshold
+## with training set, experiment with
+##  - weight vectors: init to 1, for each feature
+##  - theta: threshold, usually = 0.5
+##      -- but also n / 2 is good
 ##  - alpha: modifier (> 1)
+
+theta = 0.5
+alpha = 2
+
+def weighted_sum(row, weights):
+    acc = 0
+    row = row.drop('class') # class does not factor into weights
+    for idx, val in enumerate(row.to_numpy()):
+        acc += val * (weights[idx])
+    return acc
+
+# def promote(row, weights, alpha):
+
+# divide weights of attr = 1
+def demote(row, weights, alpha):
+    row = row.drop('class')
+    new_weights = weights.copy()
+    for idx, val in enumerate(row.to_numpy()):
+        if (val == 1):
+           new_weights[idx] = weights[idx] / alpha
+    
+    return new_weights
+
+def build_table(df):
+    label = 'class'
+    n = len(df.columns.tolist()) - 1
+    weights = [1.0] * n
+
+    for _, row in df.iterrows():
+        ws = weighted_sum(row, weights)
+
+        # h(x) = 0
+        if (ws <= theta):
+            if row[label] == 1:
+                weights = demote(row, weights, alpha)
+        
+        # h(x) = 1
+        else:
+            if row[label] == 0:
+                print('false negative, demote')
+                weights = demote(row, weights, alpha)
+    
+    print(weights)
