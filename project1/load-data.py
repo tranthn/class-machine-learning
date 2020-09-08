a74a5863ed70e62a42e49cd0a03a54558e3c72f1
Emily Tran <tran.thn@gmail.com>
2020-09-07 21:09:40 -0400

    fix one issue with soy data, split all data into sets, start winnow2 notes

--- project1/load-data.py
+++ project1/load-data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import csv
+from fractions import Fraction
 import numpy as np
 import pandas as pd
 
@@ -36,9 +37,10 @@ def bin_continuous(df, bin_fields):
 
 def split_tuning_training_data(df):
     tuning = df.sample(frac = 0.1)
-    remainder = df.drop(tuning.index)
-    train = remainder.sample(frac = 0.666667)
-    test = remainder.drop(train.index)
+    df = df.drop(tuning.index)
+    train = df.sample(frac = 0.6)
+    print(train)
+    test = df.drop(train.index)
     return {'tuning': tuning, 'train': train, 'test': test}
 
 ############### main ###############
@@ -54,16 +56,11 @@ bdf3 = bdf2.copy()
 bin_continuous(bdf3, bin_fields)
 bdf4 = pd.get_dummies(bdf3)
 
-# for c in bdf4.columns.tolist():
-#     print(c)
+# print('data length', len(bdf4))
+# data_sets = split_tuning_training_data(bdf4)
+# print('data length, split', len(data_sets['tuning']), len(data_sets['train']), len(data_sets['test']))
+# print('---')
 
-print('data length', len(bdf4))
-data_sets = split_tuning_training_data(bdf4)
-print('data length, split', len(data_sets['tuning']), len(data_sets['train']), len(data_sets['test']))
-
-print('---')
-
-"""
 ## attribute values need values binned into ranges (except id, type)
 ## missing: none
 glass_fields = ['id','ri','na','mg', 'al','si','k','ca','ba','fe','type']
@@ -72,8 +69,9 @@ gdf = read_csv(glass, glass_fields)
 gdf2 = gdf.copy()
 bin_continuous(gdf2, bin_fields)
 gdf3 = pd.get_dummies(gdf2)
-for c in gdf3.columns.tolist():
-    print(c)
+print('data length', len(gdf3))
+data_sets = split_tuning_training_data(gdf3)
+print('data length, split', len(data_sets['tuning']), len(data_sets['train']), len(data_sets['test']))
 print('---')
 
 ## attribute values need values binned into ranges (except class)
@@ -83,17 +81,19 @@ bin_fields = iris_fields[:-1]
 irdf = read_csv(iris, iris_fields)
 irdf2 = irdf.copy()
 bin_continuous(irdf2, bin_fields)
-for c in irdf2.columns.tolist():
-    print(c)
+print('data length', len(irdf2))
+data_sets = split_tuning_training_data(irdf2)
+print('data length, split', len(data_sets['tuning']), len(data_sets['train']), len(data_sets['test']))
 print('---')
 
 ## attribute values are either multi-categorical or binary (assuming no missing)
 ## missing: none
-soybean_fields = ['date','plant-stand','precip','temp','hail','crop-hist','area-damaged','severity','seed-tmt','germination','plant-growth','leaves','leafspots-halo','leafspots-marg','leafspot-size','leaf-shread','leaf-malf','leaf-mild','stem','lodging','stem-cankers','canker-lesion','fruiting-bodies','external decay','mycelium','int-discolor','sclerotia','fruit-pods','fruit spots','seed','mold-growth','seed-discolor','seed-size','shriveling','roots']
+soybean_fields = ['date','plant-stand','precip','temp','hail','crop-hist','area-damaged','severity','seed-tmt','germination','plant-growth','leaves','leafspots-halo','leafspots-marg','leafspot-size','leaf-shread','leaf-malf','leaf-mild','stem','lodging','stem-cankers','canker-lesion','fruiting-bodies','external decay','mycelium','int-discolor','sclerotia','fruit-pods','fruit spots','seed','mold-growth','seed-discolor','seed-size','shriveling','roots', 'class']
 soydf = read_csv(soybean, soybean_fields)
 soydf2 = pd.get_dummies(soydf)
-for c in soydf2.columns.tolist():
-    print(c)
+print('data length', len(soydf2))
+data_sets = split_tuning_training_data(soydf2)
+print('data length, split', len(data_sets['tuning']), len(data_sets['train']), len(data_sets['test']))
 print('---')
 
 ## attribute values are either multi-categorical or binary (assuming no missing)
@@ -102,7 +102,7 @@ print('---')
 house_fields = ['class', 'handicapped-infants', 'water-project-cost-sharing', 'adoption-of-the-budget-resolution', 'physician-fee-freeze', 'el-salvador-aid', 'religious-groups-in-schools', 'anti-satellite-test-ban', 'aid-to-nicaraguan-contras', 'mx-missile', 'immigration', 'synfuels-corporation-cutback', 'education-spending', 'superfund-right-to-sue', 'crime', 'duty-free-exports', 'export-administration-act-south-africa']
 housedf = read_csv(house, house_fields)
 housedf2 = pd.get_dummies(housedf)
-for c in housedf2.columns.tolist():
-    print(c)
+print('data length', len(housedf))
+data_sets = split_tuning_training_data(housedf)
+print('data length, split', len(data_sets['tuning']), len(data_sets['train']), len(data_sets['test']))
 print('---')
-"""
---
+++ project1/winnow2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+import csv
+import numpy as np
+import pandas as pd
+
+## winnow steps
+## with tuning set, experiment with
+##  - weight vectors
+##  - theta: threshold
+##  - alpha: modifier (> 1)
