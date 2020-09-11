b34244c03e0780a9db53143fdde3070f655e7e40
Emily Tran <tran.thn@gmail.com>
2020-09-11 13:55:29 -0400

    Add logic for bayes with multinomial classes, update table calls in load-data

--- project1/bayes.py
+++ project1/bayes.py
@@ -7,26 +7,43 @@ import pandas as pd
 m = 1
 p = 0.0001
 
-def build_probability_table(df):
-    label = 'class'
+def build_probability_table(df, label, is_multinomial = False):
     n = df.shape[0]
 
     ## create data frame to store probabilities
     ## dimensions = # classes x (# features + 2)
     ## each column = P(f = 0 | c), since the symmetrical f-value (f = 1) will just be [1 - P(f = 0 | c)]
-    df2 = df.copy()
-    df2 = df2.groupby([label]).size()
+    probdf = pd.DataFrame()
 
-    cols = df.copy().drop(columns = label).columns.tolist()
-    classes = df['class'].value_counts()
-    probdf = pd.DataFrame(0, index = classes.index, columns = cols)
+    if (is_multinomial == False):
+        
+        cols = df.copy().drop(columns = label).columns.tolist()
+        classes = df[label].value_counts()
+        probdf = pd.DataFrame(0, index = classes.index, columns = cols)
+        
+        for c in classes.index:
+            for f in cols:
+                df2 = df.copy()
+                # probability that feature = 0
+                # proability that feature = 1 will just be 1 - p0
+                p0 = (df2[f].values == 0).sum()
+                probdf.loc[c,f] = p0 / n
     
-    for c in classes.index:
-        for f in cols:
-            df2 = df.copy()
-            # probability that feature = 0
-            # proability that feature = 1 will just be 1 - p0
-            p0 = (df2[f].values == 0).sum()
-            probdf.loc[c,f] = p0 / n
+    else:
+        classes = [col for col in df if col.startswith(label)]
+        classes = np.array(classes)
+        cols = df.copy().drop(columns = classes).columns.tolist()
 
-    print(probdf)
+        print(classes)
+
+        probdf = pd.DataFrame(0, index = classes, columns = cols)
+        
+        for c in classes:
+            for f in cols:
+                df2 = df.copy()
+                # probability that feature = 0
+                # proability that feature = 1 will just be 1 - p0
+                p0 = (df2[f].values == 0).sum()
+                probdf.loc[c,f] = p0 / n
+
+    # print(probdf)
--- project1/load-data.py
+++ project1/load-data.py
@@ -65,14 +65,15 @@ data_sets = split_tuning_training_data(bdf4)
 wts = win.build_table(df = data_sets['train'], label = 'class')
 win.test_model(data_sets['tuning'], wts)
 
-# # naive bayes
-# # -----------------
-pt = bayes.build_probability_table(data_sets['train'])
+# naive bayes
+# -----------------
+pt = bayes.build_probability_table(data_sets['train'], label = 'class')
 
 ########################################
 ## attribute values need values binned into ranges (except id, class)
 ## missing: none
 ## class: 6 options
+print ('-- GLASS DATA -- ')
 glass_fields = ['id','ri','na','mg', 'al','si','k','ca','ba','fe','class']
 bin_fields = glass_fields[1:-1]
 gdf = read_csv(glass, glass_fields)
@@ -80,41 +81,70 @@ gdf2 = gdf.copy().astype({'class': object})
 bin_continuous(gdf2, bin_fields)
 gdf3 = pd.get_dummies(gdf2).drop(columns = 'id')
 data_sets = split_tuning_training_data(gdf3)
-print(data_sets['tuning'])
 
-multi_tables = win.build_table_multinomial(df = gdf3, label = 'class')
-print(len(multi_tables))
+# winnow2
+# -----------------
+multi_tables = win.build_table_multinomial(df = data_sets['train'], label = 'class')
+
+# naive bayes
+# -----------------
+pt = bayes.build_probability_table(data_sets['train'], label = 'class', is_multinomial = True)
 
 ########################################
 ## attribute values need values binned into ranges (except class)
 ## missing: none
 ## class: 3 options
+print ('-- IRIS DATA -- ')
 iris_fields = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 bin_fields = iris_fields[:-1]
 irdf = read_csv(iris, iris_fields)
 irdf2 = irdf.copy()
 bin_continuous(irdf2, bin_fields)
-irdf3 = pd.get_dummies(irdf2, columns = ['class'])
-data_sets = split_tuning_training_data(irdf2)
-# print(data_sets['tuning'])
+irdf3 = pd.get_dummies(irdf2)
+data_sets = split_tuning_training_data(irdf3)
+
+# winnow2
+# -----------------
+multi_tables = win.build_table_multinomial(df = data_sets['train'], label = 'class')
+
+# naive bayes
+# -----------------
+pt = bayes.build_probability_table(data_sets['train'], label = 'class', is_multinomial = True)
 
 ########################################
 ## attribute values are either multi-categorical or binary (assuming no missing)
 ## missing: none
 ## class: 4 options
+print ('-- SOYBEAN DATA -- ')
 soybean_fields = ['date','plant-stand','precip','temp','hail','crop-hist','area-damaged','severity','seed-tmt','germination','plant-growth','leaves','leafspots-halo','leafspots-marg','leafspot-size','leaf-shread','leaf-malf','leaf-mild','stem','lodging','stem-cankers','canker-lesion','fruiting-bodies','external decay','mycelium','int-discolor','sclerotia','fruit-pods','fruit spots','seed','mold-growth','seed-discolor','seed-size','shriveling','roots', 'class']
 soydf = read_csv(soybean, soybean_fields)
 soydf2 = pd.get_dummies(soydf)
 data_sets = split_tuning_training_data(soydf2)
-# print(data_sets['tuning'])
+
+# winnow2
+# -----------------
+multi_tables = win.build_table_multinomial(df = data_sets['train'], label = 'class')
+
+# naive bayes
+# -----------------
+pt = bayes.build_probability_table(data_sets['train'], label = 'class', is_multinomial = True)
 
 ########################################
 ## attribute values are either multi-categorical or binary (assuming no missing)
 ## ? = abstain, not missing values
 ## missing: none
 ## class: 2 options
+print ('-- HOUSE DATA -- ')
 house_fields = ['class', 'handicapped-infants', 'water-project-cost-sharing', 'adoption-of-the-budget-resolution', 'physician-fee-freeze', 'el-salvador-aid', 'religious-groups-in-schools', 'anti-satellite-test-ban', 'aid-to-nicaraguan-contras', 'mx-missile', 'immigration', 'synfuels-corporation-cutback', 'education-spending', 'superfund-right-to-sue', 'crime', 'duty-free-exports', 'export-administration-act-south-africa']
 housedf = read_csv(house, house_fields)
 housedf2 = pd.get_dummies(housedf)
 data_sets = split_tuning_training_data(housedf)
-# print(data_sets['tuning'])
+
+# winnow2
+# -----------------
+wts = win.build_table(df = data_sets['train'], label = 'class')
+win.test_model(data_sets['tuning'], wts)
+
+# naive bayes
+# -----------------
+pt = bayes.build_probability_table(data_sets['train'], label = 'class')
